6b9f21d4c99e3cfde12dd3a9b1cace579198d709
anjali790 <anjaliruby790@gmail.com>
2022-11-30 23:24:00 +0530

    quiz app

--- src/components/QuizApp/QuizApp.jsx
+++ src/components/QuizApp/QuizApp.jsx
@@ -1,50 +1,13 @@
 import React, { useState } from 'react';
 
+import { questions } from './questions';
+
 import './quizApp.css';
 
 export function QuizApp() {
     const [currentQuestion, setCurrentQuestion] = useState(0);
     const [showScore, setShowScore] = useState(false);
     const [score, setScore] = useState(0);
-
-    const questions = [
-        {
-            questionText: 'What is not a wind instrument?',
-            answerOptions: [
-                { answerText: 'Oboe', isCorrect: false },
-                { answerText: 'Trombone', isCorrect: false },
-                { answerText: 'Viola', isCorrect: true },
-                { answerText: 'Duduk', isCorrect: false },
-            ],
-        },
-        {
-            questionText: 'What was the name of the WWF professional wrestling tag team made up of the wrestlers Ax and Smash?',
-            answerOptions: [
-                { answerText: 'The Dream Team', isCorrect: false },
-                { answerText: 'Demolition', isCorrect: true },
-                { answerText: 'The Bushwhackers', isCorrect: false },
-                { answerText: 'The British Bulldogs', isCorrect: false },
-            ],
-        },
-        {
-            questionText: 'Which of these is NOT a player class in Team Fortress 2?',
-            answerOptions: [
-                { answerText: 'Healer', isCorrect: true },
-                { answerText: 'Demoman', isCorrect: false },
-                { answerText: 'Pyro', isCorrect: false },
-                { answerText: 'Spy', isCorrect: false },
-            ],
-        },
-        {
-            questionText: 'How many studio albums have the duo Daft Punk released?',
-            answerOptions: [
-                { answerText: '1', isCorrect: false },
-                { answerText: '5', isCorrect: false },
-                { answerText: '2', isCorrect: false },
-                { answerText: '4', isCorrect: true },
-            ],
-        },
-    ];
 
     const handleAnswerOptionClick = (isCorrect) => {
         if (isCorrect) {
---
+++ src/components/QuizApp/questions.jsx
@@ -0,0 +1,92 @@
+export const questions = [
+    {
+        questionText: 'What is not a wind instrument?',
+        answerOptions: [
+            { answerText: 'Oboe', isCorrect: false },
+            { answerText: 'Trombone', isCorrect: false },
+            { answerText: 'Viola', isCorrect: true },
+            { answerText: 'Duduk', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'What was the name of the WWF professional wrestling tag team made up of the wrestlers Ax and Smash?',
+        answerOptions: [
+            { answerText: 'The Dream Team', isCorrect: false },
+            { answerText: 'Demolition', isCorrect: true },
+            { answerText: 'The Bushwhackers', isCorrect: false },
+            { answerText: 'The British Bulldogs', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Which of these is NOT a player class in Team Fortress 2?',
+        answerOptions: [
+            { answerText: 'Healer', isCorrect: true },
+            { answerText: 'Demoman', isCorrect: false },
+            { answerText: 'Pyro', isCorrect: false },
+            { answerText: 'Spy', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'How many studio albums have the duo Daft Punk released?',
+        answerOptions: [
+            { answerText: '1', isCorrect: false },
+            { answerText: '5', isCorrect: false },
+            { answerText: '2', isCorrect: false },
+            { answerText: '4', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'During WWII, in 1945, the United States dropped atomic bombs on the two Japanese cities of Hiroshima and what other city?',
+        answerOptions: [
+            { answerText: 'Kawasaki', isCorrect: false },
+            { answerText: 'Tokyo', isCorrect: false },
+            { answerText: 'Kagoshima', isCorrect: false },
+            { answerText: 'Nagasaki', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'Who was the original drummer for The Beatles?',
+        answerOptions: [
+            { answerText: 'Ringo Starr', isCorrect: false },
+            { answerText: 'Stuart Sutcliffe', isCorrect: false },
+            { answerText: 'Pete Best', isCorrect: false },
+            { answerText: 'Tommy Moore', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'What is the main ship used by Commander Shepard in the Mass Effect Franchise called??',
+        answerOptions: [
+            { answerText: 'Osiris', isCorrect: false },
+            { answerText: 'Infinity', isCorrect: false },
+            { answerText: 'Endeavour', isCorrect: false },
+            { answerText: 'Normandy', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'Which nation claims ownership of Antarctica?',
+        answerOptions: [
+            { answerText: 'United States of America', isCorrect: false },
+            { answerText: 'United Nations', isCorrect: false },
+            { answerText: 'Australia', isCorrect: false },
+            { answerText: 'No one, but there are claims', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'In Pokemon Red &amp; Blue, what is the name of HM05?',
+        answerOptions: [
+            { answerText: 'Strength', isCorrect: false },
+            { answerText: 'Cut', isCorrect: false },
+            { answerText: 'Fly', isCorrect: true },
+            { answerText: 'No one, but there are claims', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'How many Chaos Emeralds are there in the &quot;Sonic the Hedgehog&quot; universe?',
+        answerOptions: [
+            { answerText: '7', isCorrect: true },
+            { answerText: '14', isCorrect: false },
+            { answerText: '8', isCorrect: false },
+            { answerText: '6', isCorrect: false },
+        ],
+    },
+];
